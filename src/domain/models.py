13c26ef192cc533c03e38343fdcf043be63cd711
Charlie <charlieavery123@hotmail.co.uk>
2024-08-30 14:23:32 +0100

    adding orderbook functionality

--- src/domain/models.py
+++ src/domain/models.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from domain import exceptions
+
 
 @dataclass
 class User:
@@ -19,10 +21,16 @@ class Order:
 
 
 class OrderBook:
-    def __init__(self) -> None:
+    def __init__(self, price: int = 100) -> None:
         self.orders: set[Order] = set()
+        self.price = price
 
     def place_order(self, order: Order):
+        invalid_long = order.price >= self.price and order.quantity > 0
+        invalid_short = order.price <= self.price and order.quantity < 0
+        if order.quantity == 0 or invalid_long or invalid_short:
+            raise exceptions.InvalidOrder
+
         self.orders.add(order)
         return order
 
--- tests/domain/test_order_book.py
+++ tests/domain/test_order_book.py
@@ -1,20 +1,21 @@
-from domain.models import Order, OrderBook, User
+import pytest
+from domain import models, exceptions
 
 
 def test_limit_order_placement():
     """Test placing a limit order."""
     # Create User
-    user = User(id=1)
+    user = models.User(id=1)
 
     # Create Order
-    order = Order(
+    order = models.Order(
         id=1,
         user_id=user.id,
         quantity=1,
-        price=100,
+        price=90,
     )
 
-    order_book = OrderBook()
+    order_book = models.OrderBook()
 
     # Place a limit order
     placed_order = order_book.place_order(order)
@@ -24,5 +25,74 @@ def test_limit_order_placement():
 
     order = order_book.get_order(id=placed_order.id)
     assert order is not None
-    assert order.price == 100
+    assert order.price == 90
     assert order.quantity == 1
+
+
+def test_buy_order_above_market():
+    """
+    Test placing buy order above market price
+    (this should be a stop order later on)
+    """
+    # Create User
+    user = models.User(id=1)
+
+    # Create Order
+    order = models.Order(
+        id=1,
+        user_id=user.id,
+        quantity=1,
+        price=100,
+    )
+
+    order_book = models.OrderBook(price=90)
+
+    with pytest.raises(exceptions.InvalidOrder):
+        # Place invalid order
+        order_book.place_order(order)
+
+
+def test_sell_order_below_market():
+    """
+    Test placing buy order above market price
+    (this should be a stop order later on)
+    """
+    # Create User
+    user = models.User(id=1)
+
+    # Create Order
+    order = models.Order(
+        id=1,
+        user_id=user.id,
+        quantity=-1,
+        price=80,
+    )
+
+    order_book = models.OrderBook(price=90)
+
+    with pytest.raises(exceptions.InvalidOrder):
+        # Place invalid order
+        order_book.place_order(order)
+
+
+def test_zero_order():
+    """
+    Test placing buy order above market price
+    (this should be a stop order later on)
+    """
+    # Create User
+    user = models.User(id=1)
+
+    # Create Order
+    order = models.Order(
+        id=1,
+        user_id=user.id,
+        quantity=0,
+        price=80,
+    )
+
+    order_book = models.OrderBook(price=90)
+
+    with pytest.raises(exceptions.InvalidOrder):
+        # Place invalid order
+        order_book.place_order(order)
