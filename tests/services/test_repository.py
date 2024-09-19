bb0188c86e6976fbe2be26f5ca70422ffc336ce2
Charlie <charlieavery123@hotmail.co.uk>
2024-09-19 21:30:32 +0900

    add xfail to unprepared tests

--- tests/services/test_repository.py
+++ tests/services/test_repository.py
@@ -1,6 +1,11 @@
+import pytest
+
+
+@pytest.mark.xfail
 def test_repository_can_retrieve_order_book():
     raise NotImplementedError
 
 
+@pytest.mark.xfail
 def test_repository_can_add_order():
     raise NotImplementedError
